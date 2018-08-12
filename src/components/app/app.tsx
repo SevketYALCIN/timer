5d89acc1803823542e3e598158ddb98a68c82b21
Sevket <sevket.yalcin@outlook.com>
2018-08-12 23:32:39 +0900

    feat: close button on pane

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,47 +1,50 @@
-import * as React from "react";
-import Timer from "../timer/timer";
-import Pane from "../pane/pane";
-import "./app.scss";
-import * as Menu from "../../assets/menu.svg";
+import * as React from 'react';
+import Timer from '../timer/timer';
+import Pane from '../pane/pane';
+import './app.scss';
+import * as Menu from '../../assets/menu.svg';
 
 class App extends React.Component<null, AppState> {
   constructor() {
-    super(null)
+    super(null);
     this.state = {
       schedule: null,
       showPane: false
-    }
+    };
   }
 
   updateSchedule = (newSchedule: ScheduleItem[]) => {
     this.setState({
       schedule: newSchedule
-    })
-  }
+    });
+  };
 
   showMenu = () => {
     this.setState({
       showPane: true
-    })
-  }
+    });
+  };
 
   hideMenu = () => {
     this.setState({
       showPane: false
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        { this.state.showPane && <div id="blurBackground" onClick={this.hideMenu} /> }
+        {this.state.showPane && (
+          <div id="blurBackground" onClick={this.hideMenu} />
+        )}
         <img id="menuImg" src={Menu} onClick={this.showMenu} />
-        <Timer
-          minute={5}
-          second={0}
+        <Timer minute={5} second={0} schedule={this.state.schedule} />
+        <Pane
+          close={() => this.setState({ showPane: false })}
+          active={this.state.showPane}
+          onChange={this.updateSchedule}
           schedule={this.state.schedule}
         />
-        <Pane active={this.state.showPane} onChange={this.updateSchedule} schedule={this.state.schedule}/>
       </div>
     );
   }
--- src/components/pane/pane.tsx
+++ src/components/pane/pane.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import "./pane.scss";
 import { ScheduleItem } from "../app/app";
+import * as Cross from "../../assets/cross.svg";
 
-export default class Pane extends React.Component<PaneProps> {
+export default class Pane extends React.Component<PaneProps, PaneState> {
   constructor(props: PaneProps) {
     super(props)
     this.state = {
@@ -13,6 +14,7 @@ export default class Pane extends React.Component<PaneProps> {
   render() {
     return (
       <div className={this.props.active ? "pane active" : "pane"}>
+        <img id="close" src={Cross} onClick={this.props.close} />
       </div>
     );
   }
@@ -22,4 +24,9 @@ interface PaneProps {
   schedule?: ScheduleItem[];
   onChange: (newSchedule:ScheduleItem[]) => void;
   active: boolean;
+  close: () => void;
+}
+
+interface PaneState {
+  schedule?: ScheduleItem[];
 }
